56e5a92ffc24baf1a2bdb76b4d2727cba3b06682
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-11 20:59:55 +0530

    Update guvi4.py

--- guvi4.py
+++ guvi4.py
@@ -1,5 +1,7 @@
 n=char(input())
-if(n=='A" or n=="a",n=="E" or n=="e",n=="I" or n=="i",n=="O" or n=="o",n=="U" or n=="u"):
+if(n>=0):
+	print(n, "is a invalid")
+elif(n=='A" or n=="a",n=="E" or n=="e",n=="I" or n=="i",n=="O" or n=="o",n=="U" or n=="u"):
 	print(n," is a vowel")
 else:
 	print(n,"is a constsnt")
